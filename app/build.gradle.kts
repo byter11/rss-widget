a5f55879856bfd73127f7d1d4debc5843fdab6df
byter11 <mohsinshk4026@gmail.com>
2026-07-05 20:03:26 +0500

    Add Main Activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.byterdevs.rsswidget"
         minSdk = 28
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.4.1"
+        versionCode = 7
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
